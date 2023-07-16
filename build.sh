a7d94c25a4e72802301ad6368f7906a8556991bf
vlpolina <vlasova2002polina18@gmail.com>
2023-07-17 00:15:59 +0700

    changesettings

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-apt-get update
-apt-get install -y python3.8-venv
-python3.8 -m venv venv
+sudo apt-get install python3.6-venv
+python -m venv venv
 source venv/bin/activate
 source venv/Scripts/activate
 pip install -r kvantoriumNetworks/requirements.txt
