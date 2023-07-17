06b930d117eaefcea67bf85d9bdfa260399e4659
vlpolina <vlasova2002polina18@gmail.com>
2023-07-17 18:39:05 +0700

    changepages

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get install python3.6-venv
-python -m venv venv
-source venv/bin/activate
-source venv/Scripts/activate
-pip install -r kvantoriumNetworks/requirements.txt
-python kvantoriumNetworks/manage.py collectstatic --noinput
